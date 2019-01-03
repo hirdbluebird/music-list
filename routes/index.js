1eb2396e6d062108fc7e00af67c7700369378969
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-01-03 02:54:53 +0200

    lesson 14

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'MusicList Alpha' });
+});
+
+const testJSON = [
+  {
+    name: 'John Smith',
+    username: 'CaptainCode'
+  },
+  {
+    name: 'Jane Doe',
+    username: 'JaneyCakes'
+  }
+];
+
+router.get('/sendjson', (res, req, next) => {
+  res.json(testJSON);
 });
 
 module.exports = router;
