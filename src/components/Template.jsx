8fd7562de576c44fbb317894d8df79f24c15fba3
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-01-06 09:57:48 +0200

    react folders and template

---
+++ src/components/Template.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Template() {
+  return (
+    <div className="wrapper">
+      <header>
+        <h1>MusicList</h1>
+      </header>
+      <div className="content">Content wull eventually go here</div>
+    </div>
+  );
+}
