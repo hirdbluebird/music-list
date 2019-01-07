454bbea73c81226bdd3d3ce97d80aa754748d2a5
hirdbluebird <bluebirdyourdestiny@gmail.com>
2019-01-07 09:13:12 +0200

    DRY

--- src/components/account/ProfilePage.jsx
+++ src/components/account/ProfilePage.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
+import Sidebar from '../shared/Sidebar';
+
 export default function HomePage(props) {
   const { match } = props;
   return (
-    <section className="page-content">
-      <div className="row">
-        <div className="col-sm-12 col-lg-8">
-          {`This is profile page. The profile ID is: ${match.params.id}`}
-        </div>
-        <aside className="col-sm-12 col-lg-8">
-          {'This is the sidebar'}
-        </aside>
+    <div className="row">
+      <div className="col-sm-12 col-lg-8">
+        {`This is profile page. The profile ID is: ${match.params.id}`}
       </div>
-    </section>
+      <Sidebar />
+    </div>
   );
 }
---
+++ src/components/shared/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Sidebar() {
+  return (
+    <aside className="col-sm-12 col-lg-4">
+      {'This is new sidebar component'}
+    </aside>
+  );
+}
